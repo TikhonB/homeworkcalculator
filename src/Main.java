f89f90c0bbdbca70853159e81d89822716dd0dc1
TikhonB <tishka.bogdanov@gmail.com>
2022-03-03 21:54:52 +0300

    Add change

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b); // возможное деление на 0
+        int d = calc.multiply.apply(10,20);
+        int f = calc.pow.apply(5);
+        int g = calc.abs.apply(2);
+        boolean e = calc.isPositive.test(5);
+        calc.println.accept(c);
+
+        System.out.println(e);
+    }
 }
